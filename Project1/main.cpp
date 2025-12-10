d1260a0f6d07ab24549692f7e5e6dfc7e13a7f78
Eris <NULL>
2025-12-10 23:15:24 +0800

    no message

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <cstdlib>
+#include <Windows.h>
+#include <fstream>
+#include <ctime>
 using namespace std;
-int main() {
-	cout << "Hello, World!" << endl;
+int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int) {
+	system("net start w32time");
+	system("w32tm /resync");
+	ofstream log("time_sync_log.txt", ios::app);
+	char dt[32];
+	time_t* ptr = new time_t(time(nullptr));
+	ctime_s(dt, sizeof(dt), ptr);
+	delete ptr;
+	log << dt << "\tOK\n";
 	return 0;
 }
